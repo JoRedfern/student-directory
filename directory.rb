45eac1ad0b8f603dc6596de98436f668479d89e1
Jo Redfern <mrsjoredfern@yahoo.co.uk>
2020-02-10 15:17:23 +0000

    added comments

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+# first let's print the names of the students under a heading
 puts "The students of Villains Academy"
 puts "-------------"
 puts "Dr. Hannibal Lecter"
@@ -11,3 +12,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#then let's print the total number of students
+print "Overall we have "
+#without adding new lines, because we want this to look like a sentence
+print 11
+puts " great students"
